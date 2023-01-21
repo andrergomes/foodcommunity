fe643fd81908b7e942964006041ea42984ec48be
André Gomes <andrergomes@gmail.com>
2023-01-21 01:21:45 -0300

    Initial commit

---
+++ src/main/java/br/com/andrergomes/foodcommunity/FoodcommunityApplication.java
@@ -0,0 +1,13 @@
+package br.com.andrergomes.foodcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andrergomes/foodcommunity/FoodcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrergomes.foodcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
